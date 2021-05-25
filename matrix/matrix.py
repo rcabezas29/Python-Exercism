2d06f8d78a49197c4c1b0c6bd428613101c83ccd
Raúl Cabezas González <58788516+rcabezas29@users.noreply.github.com>
2021-05-25 19:35:15 +0200

    Update matrix.py

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,15 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix = []
-        rows = matrix_string.split("\n")
-        for a in rows:
-            self.matrix.append(a.split(" "))
+        self.matrix = [[int(n) for n in line.split()] for line in matrix_string.splitlines()]
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        column = []
-        for row in self.matrix:
-            column.append(row[index - 1])
-        return column
+        return [column[index - 1] for column in self.matrix]
